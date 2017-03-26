ace8c06764e5c2fc0f9d13e453ff865edd309abb
salad7 <msalad@uncc.edu>
2017-03-26 15:27:51 -0400

    setup onStop and on Resume Need add ToolBar to xml

--- app/src/main/java/com/example/mohamed/gsuhack/MainActivity.java
+++ app/src/main/java/com/example/mohamed/gsuhack/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.mohamed.gsuhack;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.location.Location;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,57 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.scoreID);
         Location location = new Location("Mohamed");
         tv.setText(location.getLatitude()+"");
+        String[] captions = {"Apple","Orange","Banana"};
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,captions));
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open,R.string.close){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActionBar().setTitle("Closed");
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("Open");
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(listView);
+        menu.findItem(R.id.scoreID).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
