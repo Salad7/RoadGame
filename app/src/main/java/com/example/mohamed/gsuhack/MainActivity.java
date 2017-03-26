996740dc3ee05d05d6b6db42a08be97561c8f464
salad7 <msalad@uncc.edu>
2017-03-26 14:49:33 -0400

    setup onStop and on Resume

--- app/src/main/java/com/example/mohamed/gsuhack/MainActivity.java
+++ app/src/main/java/com/example/mohamed/gsuhack/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.mohamed.gsuhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.location.Location;
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.scoreID);
+        Location location = new Location("Mohamed");
+        tv.setText(location.getLatitude()+"");
     }
+
+    @Override
+    protected void onStop() { // Works
+        super.onStop();
+        int add = 0;
+        tv.setText("OnStop");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        tv.setText("You have earned points for driving safe");
+    }
+
+
 }
